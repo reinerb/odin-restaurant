53c82fa597f711e688e11f80b36dbc66d491de78
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-04-07 14:12:23 -0400

    Add homepage via Javascript

---
+++ src/home.js
@@ -0,0 +1,44 @@
+import Booth from "./images/booth.jpg";
+
+// Create the title
+const titleDiv = document.createElement("div");
+titleDiv.classList.add("title-container");
+titleDiv.innerHTML = "<h1>Lorem Ipsum Dining, Inc</h1>";
+
+// Create an array of the other elements on the page
+
+// Introductory paragraph
+const introP = document.createElement("div");
+introP.classList.add("content-container");
+introP.innerHTML =
+  "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium non soluta sed maxime? Consequuntur exercitationem sit, sapiente corporis voluptatibus officia perferendis ab aliquam ad quos sunt dicta placeat modi excepturi.</p>";
+
+// Quote
+const quote = document.createElement("div");
+quote.classList.add("content-container");
+quote.innerHTML =
+  "<figure><blockquote>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facilis quis temporibus aspernatur, quas esse voluptates odit alias? Aut, error libero? Debitis deleniti sapiente dolore nostrum ab laborum eveniet recusandae?</blockquote> <figcaption>Abraham Lincoln</figcaption></figure>";
+
+// Hours
+const hours = document.createElement("div");
+hours.classList.add("content-container", "centered-text");
+hours.innerHTML =
+  "<h2>Hours</h2><ul><li>Sunday: 6am - 3pm</li><li>Monday: 6am - 5pm</li><li>Tuesday: 6am - 5pm</li><li>Wednesday: 6am - 5pm</li><li>Thursday: 6am - 5pm</li><li>Friday: 6am - 10pm</li><li>Saturday: 6am - 10pm</li></ul>";
+
+// Bottom image
+const bottomImage = document.createElement("div");
+bottomImage.classList.add("image-container");
+const boothImage = new Image();
+boothImage.src = Booth;
+bottomImage.appendChild(boothImage);
+
+// Function to display the home page in the given div
+const displayHome = function (parentDiv) {
+  parentDiv.appendChild(titleDiv);
+  parentDiv.appendChild(introP);
+  parentDiv.appendChild(quote);
+  parentDiv.appendChild(hours);
+  parentDiv.appendChild(bottomImage);
+};
+
+export default displayHome;
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import "./style.css";
+import displayHome from "./home.js";
+
+const homeTab = document.querySelector("#home");
+const menuTab = document.querySelector("#menu");
+const contactTab = document.querySelector("#contact");
+const contentDiv = document.querySelector(".content");
+
+const tabs = [homeTab, menuTab, contactTab];
+
+// Clears all children of the given div
+const clear = function (parent) {
+  while (parent.firstChild) {
+    parent.removeElement(parent.firstChild);
+  }
+};
+
+// Changes to the given tab on click
+const changeTab = function (index) {
+  for (let i = 0; i < tabs.length; i++) {
+    if (i === index) {
+      tabs[i].classList.add("active");
+    } else {
+      tabs[i].classList.remove("active");
+    }
+  }
+
+  // if (index == 0) {
+  //   clear(contentDiv);
+  //   displayHome(contentDiv);
+  // } else if (index == 1) {
+  //   clear(contentDiv);
+  //   displayMenu(contentDiv);
+  // } else if (index == 2) {
+  //   clear(contentDiv);
+  //   displayContact(contentDiv);
+  // }
+};
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", () => {
+    changeTab(i);
+  });
+}
+
+displayHome(contentDiv);
